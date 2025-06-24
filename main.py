3e340bc68b84f252f1658adced42fca2d4c7d8fb
zina1104 <zina1104@naver.com>
2025-06-24 19:22:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import streamlit as st
 import plotly.express as px
 import pandas as pd
 
+# 데이터프레임
 df = pd.DataFrame({
     '장소': ['경복궁', '남산타워'],
     '위도': [37.5702, 37.5512],
     '경도': [126.992, 126.9882]
 })
 
+# Plotly 지도 생성
 fig = px.scatter_mapbox(df, lat='위도', lon='경도', text='장소',
                         zoom=10, height=500)
-fig.update_layout(mapbox_style="open-street-map")  # 무료 스타일
-fig.show()
+
+# 무료 지도 스타일 사용
+fig.update_layout(mapbox_style="open-street-map")
+
+# ✅
