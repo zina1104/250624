7dba98205d255cf2c66c7d015182b9230c4af6f4
zina1104 <zina1104@naver.com>
2025-06-24 19:25:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
+st.title("서울 주요 관광지 지도")
+
 # 데이터프레임
 df = pd.DataFrame({
     '장소': ['경복궁', '남산타워'],
@@ -11,9 +13,10 @@ df = pd.DataFrame({
 
 # Plotly 지도 생성
 fig = px.scatter_mapbox(df, lat='위도', lon='경도', text='장소',
-                        zoom=10, height=500)
+                        zoom=11, height=600)
 
 # 무료 지도 스타일 사용
 fig.update_layout(mapbox_style="open-street-map")
 
-# ✅
+# ✅ Streamlit에서 출력
+st.plotly_chart(fig)
