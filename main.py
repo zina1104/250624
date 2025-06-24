f04396bed551c77e4c271cc79369563d5349ee5f
zina1104 <zina1104@naver.com>
2025-06-24 19:42:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ visited_places = ['강릉시', '제주시', '순천시']
 m = folium.Map(location=[36.5, 127.8], zoom_start=7)
 
 # 행정구역 GeoJSON 파일 로드 (시군구)
-with open('skorea_municipalities_geo.json', encoding='utf-8') as f:
+with open('skorea_municipalities_geo_simple.json', encoding='utf-8') as f:
     geo_data = json.load(f)
 
 # 색칠
